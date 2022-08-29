5618e0f88423d1f1b49abc23b2ab99ab4099b432
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-08-29 14:47:41 +0800

    Refactored server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-const XLSX = require('xlsx');
-const workbook = XLSX.readFile('CAMPO-DIRECTORY.xlsx', {type: "binary", cellDates: true, dateNF: 'mm/dd/yyyy;@'});
+const XLSX = require("xlsx");
+const workbook = XLSX.readFile("CAMPO-DIRECTORY.xlsx", {
+  type: "binary",
+  cellDates: true,
+  dateNF: "mm/dd/yyyy;@",
+});
 const sheet_name_list = workbook.SheetNames;
 
 const PORT = 3002;
@@ -14,58 +18,64 @@ app.use(cors());
 /* functions list */
 
 const hasSearch = (a, b) => {
-    let checker = false;
-    for(let i=0; i<a.length-b.length+1; i++){
-        if(a.slice(i, i+b.length) == b){
-            checker = true;
-            break;
-        }
+  let checker = false;
+  for (let i = 0; i < a.length - b.length + 1; i++) {
+    if (a.slice(i, i + b.length).toLowerCase() === b) {
+      checker = true;
+      break;
     }
-    return checker;
+  }
+  return checker;
 };
 
 /* end of functions list */
 
-app.post('/search', (req, res) => {
-    let toSearch = "";
-    const property = req.body.property;
-    if(property != "name") {
-        toSearch = new Date(req.body.value.slice(0,4), parseInt(req.body.value.slice(5,7))-1, parseInt(req.body.value.slice(8,10)));
-    } else {
-        toSearch = req.body.value;
-    }
-    var sheet_list = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-    if(property == "name") {
-        var ans = sheet_list.filter(val => {
-            if(val.hasOwnProperty("Name ") == false){
-                return false;
-            }
-            return hasSearch(val["Name "], toSearch);
-        });
-        res.send(ans);
-    } else if(property == "birthDate") {
-        var ans = sheet_list.filter(val => {
-            if(val.hasOwnProperty("Date of Birth") == false){
-                return false;
-            }
-            return val["Date of Birth"].getTime()-toSearch.getTime() == 0;
-        });
-        res.send(ans);
-    } else {
-        var ans = sheet_list.filter(val => {
-            if(val.hasOwnProperty("Date of Death") == false){
-                return false;
-            }
-            return val["Date of Death"].getTime()-toSearch.getTime() == 0;
-        });
-        res.send(ans);
-    }
+app.post("/search", (req, res) => {
+  let toSearch = "";
+  const property = req.body.property;
+  if (property != "name") {
+    toSearch = new Date(
+      req.body.value.slice(0, 4),
+      parseInt(req.body.value.slice(5, 7)) - 1,
+      parseInt(req.body.value.slice(8, 10))
+    );
+  } else {
+    toSearch = req.body.value;
+  }
+  let sheet_list = XLSX.utils.sheet_to_json(
+    workbook.Sheets[sheet_name_list[0]]
+  );
+  if (property == "name") {
+    let ans = sheet_list.filter((val) => {
+      if (val.hasOwnProperty("Name ") == false) {
+        return false;
+      }
+      return hasSearch(val["Name "], toSearch);
+    });
+    res.send(ans);
+  } else if (property == "birthDate") {
+    let ans = sheet_list.filter((val) => {
+      if (val.hasOwnProperty("Date of Birth") == false) {
+        return false;
+      }
+      return val["Date of Birth"].getTime() - toSearch.getTime() == 0;
+    });
+    res.send(ans);
+  } else {
+    let ans = sheet_list.filter((val) => {
+      if (val.hasOwnProperty("Date of Death") == false) {
+        return false;
+      }
+      return val["Date of Death"].getTime() - toSearch.getTime() == 0;
+    });
+    res.send(ans);
+  }
 });
 
-app.listen(PORT, err => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(`Server running at PORT ${PORT}`);
-    }
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server running at PORT ${PORT}`);
+  }
 });
